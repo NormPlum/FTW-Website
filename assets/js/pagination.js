c55358a0d3219f379201b32b815281cd220819de
NormPlum <162703454+NormPlum@users.noreply.github.com>
2026-04-19 21:40:31 +1000

    Added pagination & filtering to TV Series page.

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -3,9 +3,13 @@ var items_per_page = 30;
 
 
 // Channels pagination.
-var channels = new List('channels', {
-  valueNames: ['title', 'date', 'count'],
-  searchColumns: ['title'],
+var channels = new List("channels", {
+  valueNames: [
+    "title",
+    "count",
+    {name: "date", attr: "data-date"},
+  ],
+  searchColumns: ["title"],
   searchDelay: search_delay,
   pagination: true,
   page: items_per_page
@@ -13,28 +17,59 @@ var channels = new List('channels', {
 
 
 // Movies pagination.
-var movies = new List('movies', {
-  valueNames: ['title', 'date', 'runtime', 'count', {name: 'genres', attr: 'data-genres'}],
-  searchColumns: ['title'],
+var movies = new List("movies", {
+  valueNames: [
+    "title",
+    "runtime",
+    "count",
+    {name: "date", attr: "data-date"},
+    {name: "genres", attr: "data-genres"},
+  ],
+  searchColumns: ["title"],
   searchDelay: search_delay,
   pagination: true,
   page: items_per_page
 });
 
-// Movies filtering.
+
+// TV Series pagination.
+var tvSeries = new List("tv-series", {
+  valueNames: [
+    "title",
+    "seasons",
+    "count",
+    {name: "date", attr: "data-date"},
+    {name: "genres", attr: "data-genres"},
+  ],
+  searchColumns: ["title"],
+  searchDelay: search_delay,
+  pagination: true,
+  page: items_per_page
+});
+
+
+// Genre filtering.
 var genreFilter = document.getElementById("genre-filter");
 genreFilter.onchange = updateList;
 
 function updateList() {
+  if (movies.items.length) {
+    movies.filter(genreMatch);
+    movies.update();
+  }
+
+  if (tvSeries.items.length) {
+    tvSeries.filter(genreMatch);
+    tvSeries.update();
+  }
+}
+
+function genreMatch(item) {
   var genre = genreFilter.value;
 
-  movies.filter(function(item) {
-    if (genre == "") {
-      return true;
-    } else {
-      return item.values().genres.indexOf(String(genre)) >= 0;
-    }
-  });
-
-  movies.update();
+  if (genre == "") {
+    return true;
+  } else {
+    return item.values().genres.indexOf(genre) >= 0;
+  }
 }
