b9baa207482f121afb0d67bf7b307c0230f20b04
NormPlum <162703454+NormPlum@users.noreply.github.com>
2026-05-05 21:24:41 +1000

    Pager is working.

--- assets/js/comb/pager.js
+++ assets/js/comb/pager.js
@@ -29,33 +29,41 @@ class Pager {
     let currentPageEnd = currentPageStart + this.comb.settings.pagerItemsPerPage;
 
     for (let i in this.comb.visibleItems()) {
-      if (i < currentPageStart || i >= currentPageEnd) {
-        $(this.comb.elements.items[i]).hide();
+      // Hide all items.
+      $(this.comb.elements.items[i]).hide();
+
+      // Then show items on the current page.
+      if (i >= currentPageStart && i < currentPageEnd) {
+        $(this.comb.elements.items[i]).show();
       }
     }
 
-    this.updatePager();
-    this.updatePager();
     this.updatePager();
   }
 
   // Update the pager links.
   updatePager() {
-    // Create new pager.
+    // Build the pager.
     let pagerList = $("<ul></ul>");
     for (let i = 0; i < this.numPages(); i++) {
-      let active = (i+1 == this.currentPage) ? ' class="comb-active"' : "";
-      pagerList.append('<li><a href="#"' + active + '>' + (i+1) + '</a></li>');
+      let page = i + 1;
+      let active = (page == this.currentPage) ? ' class="comb-active"' : "";
+      pagerList.append('<li><a href="#"' + active + ' data-page="' + page + '">' + page + '</a></li>');
     }
 
+    // Add/update the pager.
     if ($(this.comb.elements.pager).children("ul").length) {
-      // Replace the old pager.
       $(this.comb.elements.pager).children("ul").replaceWith(pagerList);
     }
     else {
-      // Add the new pager.
       $(this.comb.elements.pager).append(pagerList);
     }
+
+    // Handle click events.
+    $(this.comb.elements.pager).find("ul a").on("click", (event) => {
+      this.currentPage = $(event.currentTarget).data("page");
+      this.paginate();
+    });
   }
 
 }
