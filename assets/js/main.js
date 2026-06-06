2797c760692fcb1f135edecca972db6880a4c7bd
NormPlum <162703454+NormPlum@users.noreply.github.com>
2026-06-06 21:51:05 +1000

    Started theming and reorganising templates.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,8 @@ import Comb from "./comb/comb.js";
 
 $(document).ready(function() {
   var channels = new Comb({
-    items: "#channels section",
+    items: "#channels article",
     empty: "#empty",
-    // sort: "#sort",
-    // sortFields: {
-    //   "name": {
-    //     "order": "asc",
-    //   },
-    //   "date": {
-    //     "order": "desc",
-    //   },
-    //   "videos": {
-    //     "type": "number",
-    //     "order": "desc",
-    //     "default": true,
-    //   },
-    // },
     search: "#search",
     searchFields: [
       "name",
@@ -27,26 +13,38 @@ $(document).ready(function() {
   });
 
   var movies = new Comb({
-    items: "#movies section",
+    items: "#movies article",
     empty: "#empty",
-    // sort: "#sort",
-    // sortFields: {
-    //   "name": {
-    //     "order": "asc",
-    //   },
-    //   "date": {
-    //     "order": "desc",
-    //   },
-    //   "runtime": {
-    //     "type": "number",
-    //     "order": "desc",
-    //   },
-    //   "channels": {
-    //     "type": "number",
-    //     "order": "desc",
-    //     "default": true,
-    //   },
-    // },
+    search: "#search",
+    searchFields: [
+      "name",
+    ],
+    filter: "#filters",
+    filterFields: {
+      "genre": {},
+    },
+    pager: "#pager",
+    pagerItemsPerPage: 30,
+  });
+
+  var tvSeries = new Comb({
+    items: "#tv-series article",
+    empty: "#empty",
+    search: "#search",
+    searchFields: [
+      "name",
+    ],
+    filter: "#filters",
+    filterFields: {
+      "genre": {},
+    },
+    pager: "#pager",
+    pagerItemsPerPage: 30,
+  });
+
+  var videos = new Comb({
+    items: "#videos article",
+    empty: "#empty",
     search: "#search",
     searchFields: [
       "name",
